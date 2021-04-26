b7cf0854304801eb483b9211c0d37f68e9cfe839
Tong Yang <yang.to@husky.neu.edu>
2021-04-26 17:34:29 -0400

    1

---
+++ workspace_2020_12_TeamGlory_FinalProject 2/src/dataGenerator/CSVreader.java
@@ -0,0 +1,31 @@
+package dataGenerator;
+import java.io.*;
+
+public class CSVreader {
+	public static void readCSV() throws IOException {
+		String file = "student.csv";
+		BufferedReader reader = null;
+		String line = "";
+		
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			while((line=reader.readLine())!=null) {
+				String[] row = line.split(",");
+				
+				for(String index:row) {
+					System.out.printf("%10s",index);
+				}
+				System.out.println();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			reader.close();
+		}
+	}
+	
+	public static void main(String[] arg) throws IOException {
+		CSVreader.readCSV();
+	}
+}
